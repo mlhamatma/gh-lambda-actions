fdff4aa31cc2a11d0201312bb9ce44401f91d0d8
Mason <mason@alphawolf.com>
2021-02-27 14:27:16 -0500

    Updated the body message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export const handler = async (): Promise<any> => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from aws-chat-randy to test github AWS Lambda Function!'),
+      body: JSON.stringify('Hello again from aws-chat-randy to test github AWS Lambda Function!'),
     };
     return response;
   };
